b2207caf4f1bc4ffe0a9faa7ddc4ef0fd34d0087
tanmay goyal <tanmaygoyal888@gmail.com>
2022-08-22 18:37:07 +0530

    backend

---
+++ controllers/auth.js
@@ -0,0 +1,29 @@
+const User = require("../models/user");
+exports.createOupdateuser = async (req, res) => {
+    const {name, email, picture} = req.user;
+    
+    const user = await User.findOneAndUpdate({email}, {name, picture}, {new: true,useFindAndModify: false});
+
+    if(user){
+        res.json(user)
+        console.log(user)
+    }else{
+        const newUser = await new User({
+            email,
+            name,
+            picture,
+        }).save();
+        
+        res.json(newUser);
+        console.log(newUser)
+    }
+}
+
+
+
+exports.currentUser = async (req, res) => {
+    User.findOne({ email: req.user.email }).exec((err, user) => {
+      if (err) throw new Error(err);
+      res.json(user);
+    });
+  };
